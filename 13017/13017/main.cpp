9695046e04dc4629e0d608b445a5a4695668af34
Game110AM <Game110AM@605-110-STU21>
2017-02-06 08:21:46 -0700

    1/30/2017

---
+++ 13017/13017/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<<"Knock Knock!"<<endl;
+	cout <<  endl;
+	cout << "Whose there?" << endl;
+	cout << endl;
+	cout << "Orange" << endl;
+	cout << endl;
+	cout << "Orange who?" << endl;
+	cout << endl;
+	cout << "Orange you glad to meet me?" << endl;
+
+
+
+}
