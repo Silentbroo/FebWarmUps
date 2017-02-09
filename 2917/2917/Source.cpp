d194bfb271bc080538b9b12ae5c50d8eecec9623
Game110AM <Game110AM@605-110-STU21>
2017-02-09 07:46:08 -0700

    quotes

---
+++ 2917/2917/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	for (int i = 0; i <= 200; i++)
+cout<< "Once you meet someone you never really forget them. It takes a while for your memories to return" << endl;
+}
